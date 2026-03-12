149912fc27c5750c628b9958203fbb7dc78f1d49
MartinLindholm00 <martin@mulleland.dk>
2026-03-12 12:27:45 +0100

    Leget lidt med at kunne trække papir rundt på canvas

---
+++ BachelorProjectMN/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDrag()
+    { 
+        // Get the main camera
+        Camera cam = Camera.main;
+        if (cam == null) return;
+
+        // Distance from camera to the object's z plane
+        float distanceToObject = Mathf.Abs(transform.position.z - cam.transform.position.z);
+
+        // Convert screen point to world point at that distance
+        Vector3 mouseWorld = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToObject));
+
+        // Keep the object's original z
+        transform.position = new Vector3(mouseWorld.x, mouseWorld.y, transform.position.z);
+    }
+}
+
