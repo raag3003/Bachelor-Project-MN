398dc2e091ae76a767d9de9c6285aecd429abce2
raag3003 <144060124+raag3003@users.noreply.github.com>
2026-04-28 14:04:35 +0200

    maybe fixed touchpad bug

---
+++ BachelorProjectMN/Assets/Scripts/MouseChecker.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseChecker : MonoBehaviour
+{
+
+    void Update()
+    {
+        // Count all pointer devices (mouse, touchpad, pen)
+        int pointerCount = InputSystem.devices.Count(d => d is Pointer);
+
+        // If more than 1 pointer exists, assume external mouse is connected
+        Debug.Log(pointerCount);
+    }
+}
