43ec557bbd2fcc2b93aef8cc5cff85eb40943e31
raag3003 <144060124+raag3003@users.noreply.github.com>
2026-05-26 20:56:20 +0200

    Added break between main menu and day 1

--- BachelorProjectMN/Assets/Scripts/MainMenuScript.cs
+++ BachelorProjectMN/Assets/Scripts/MainMenuScript.cs
@@ -6,12 +6,18 @@ public class MainMenuScript : MonoBehaviour
     public void Play()
     {
         Debug.Log("Load First Scene");
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Pre-Day 1");
     }
 
     public void QuitGame()
     {
         Application.Quit();
         Debug.Log("Game is Exiting");
+    }
+
+    public void ContinueToDay1()
+    {
+        Debug.Log("Load day 1");
+        SceneManager.LoadScene("SampleScene");
     }
 }
