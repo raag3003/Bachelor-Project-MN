bea670395e929c34f0f355fd41edfa4473517d51
raag3003 <144060124+raag3003@users.noreply.github.com>
2026-05-27 16:42:50 +0200

    better mapper

--- BachelorProjectMN/Assets/Scripts/UIMainMenu.cs
+++ BachelorProjectMN/Assets/Scripts/UIMainMenu.cs
@@ -6,6 +6,13 @@ public class UIMainMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     public Vector3 originalScale = new Vector3(2f, 2f, 1f);
     public Vector3 hoverScale = new Vector3(2.2f, 2.2f, 1f);
 
+    public void Start()
+    {
+        originalScale = transform.localScale;
+        hoverScale.x = originalScale.x + 0.2f;
+        hoverScale.y = originalScale.y + 0.2f;
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         transform.localScale = hoverScale;
